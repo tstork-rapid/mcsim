b002d2833dc6529ba6c57af904aae96c97ec9d16
Taggart Stork <tstork@rapiddosimetry.com>
2025-10-20 10:32:34 -0400

    added script to create attenuation map from SIMIND density file

---
+++ create_atn.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+from os.path import exists
+from sys import exit
+from glob import glob
+import numpy as np
+import NumpyIm as npi
+from runcmd import runcmd
+
+# Check if SIMIND density map exists
+dens_map_txt = "*dens*.im"
+dens_map_files = glob(dens_map_txt)
+if len(dens_map_files) < 1:
+    print("No density map found")
+    exit(1)
+elif len(dens_map_files) > 1:
+    print("More than one density map found, using " + dens_map_files[0])
+
+# Load density map
+dens_map_file = dens_map_files[0]
+pix = npi.ArrayFromIm(dens_map_file)
+
+# Check if there is only water and air
+mask = (pix != 0) & (pix != 1000)
+has_other_values = np.any(mask)
+if has_other_values:
+    print("Script doesn't handle all densities in the image")
+    exit(1)
+
+# Convert density map to CT
+pix[pix == 0] = -1000 # convert air
+pix[pix == 1000] = 0 # convert water
+
+# Rotate CT -90 degrees about Z axis
+pix = np.rot90(pix, k=3, axes=(1, 2))
+
+# Save CT as .im
+npi.ArrayToIm(pix.astype(np.float32), "ct.im")
+
+# Reduce CT to 128x128x128 if necessary
+shape = pix.shape
+if shape[0] == 256 and shape[1] == 256 and shape[2] == 256:
+    print("Downsampling CT")
+    if exists("ct_128.im"):
+        print("Removing previous ct_128.im")
+        cmd = "rm ct_128.im"
+        print("Running: " + cmd)
+        runcmd(cmd,1)
+
+    cmd = "collapse3d -a 2 2 2 ct.im ct_128.im" # average the collapse, not sum, since it's not activity units
+    print("Running: " + cmd)
+    runcmd(cmd,1)
+
+    ct_name = "ct_128.im"
+elif shape[0] == 128 and shape[1] == 128 and shape[2] == 128:
+    print("No downsampling needed")
+    ct_name = "ct.im"
+else:
+    print("Unsupported of desnity map, shape: (" + str(shape[0]) + "," + str(shape[1]) + "," + str(shape[2]) + ")")
+    exit(1)
+
+# Create attenuation map from CT
+keVs = [218, 440]
+i = 0
+for keV in keVs:
+    i += 1
+    cmd = f"./hu2atn -e {keV} -s 0.48 {ct_name} atn.w{i}.im"
+    print("Running: " + cmd)
+    runcmd(cmd,1)
+
+# Create symbolic links
+cmd = f"ln -s atn.w1.im atn.w1i1.im"
+print("Running: " + cmd)
+runcmd(cmd,1)
+cmd = f"ln -s atn.w1.im atn.w1i2.im"
+runcmd(cmd,1)
+print("Running: " + cmd)
