e4fd27cd852ab9c72288751120d6a6f108414fc5
Taggart Stork <tstork@rapiddosimetry.com>
2025-10-29 16:12:31 -0400

    switched processing to use SIMIND mu map, not rho*1000

--- create_atn.py
+++ create_atn.py
@@ -5,13 +5,14 @@ from glob import glob
 import numpy as np
 import NumpyIm as npi
 from runcmd import runcmd, waitall
+import subprocess
 
-# TODO: check HCT and ICT interfiles from SIMIND. Scale interfile by 1 and it should output .im
 
 # Ensure a keV was input by the user
 if len(argv) < 2:
-    print("Usage: create_atn.py keV [keV 2] ... [keV n]")
-    print("Uses SIMIND density map to make attenuation maps (downsampled to 128x128x128 if needed) at the desired keVs")
+    print("Usage: create_atn.py")
+    print("Uses SIMIND attenuation map to make attenuation maps (downsampled to 128x128x128 if needed)")
+    print("MUST HAVE INDEX 22 IN SIMIND.INI SET TO 3")
     exit(1)
 
 # Make a list of keVs from user input
@@ -20,83 +21,75 @@ for i in range(1,len(argv)):
     keVs.append(float(argv[i]))
 
 # Check if SIMIND density map exists
-dens_map_txt = "*.hct"
-dens_map_files = glob(dens_map_txt)
-if len(dens_map_files) < 1:
-    print("No density map found")
+atn_map_txt = "*.hct"
+atn_map_files = glob(atn_map_txt)
+if len(atn_map_files) < 1:
+    print("No attenuation map found")
     exit(1)
-elif len(dens_map_files) > 1:
-    print("More than one density map found, using " + dens_map_files[0])
+elif len(atn_map_files) > 1:
+    print("More than one attenuation map found, using " + atn_map_files[0])
 
 # Convert .hct/.ict to .im
-dens_map_file = dens_map_files[0]
-cmd = f"imgconv -r {dens_map_file} simind_dens_map.im"
+atn_map_file = atn_map_files[0]
+cmd = f"imgconv -r {atn_map_file} simind_atn_map.im"
 print("Running: " + cmd)
 runcmd(cmd,1)
 waitall()
 
-# Load density map
-pix = npi.ArrayFromIm("simind_dens_map.im")
+# Load attenuation map
+pix = npi.ArrayFromIm("simind_atn_map.im")
 
-# Check if there is only water and air
-mask = (pix != 0) & (pix != 1000)
-has_other_values = np.any(mask)
-if has_other_values:
-    print("Script doesn't handle all densities in the image")
-    exit(1)
-
-# Convert density map to CT
-pix[pix == 0] = -1000 # convert air
-pix[pix == 1000] = 0 # convert water
+# Get bin width
+pixel_size = subprocess.check_output(["imghdr", "-i", "Pixel Size", atn_map_file])
+bin_width = pixel_size.decode('ascii').strip().split(" ")
 
 # Rotate CT -90 degrees about Z axis
 pix = np.rot90(pix, k=3, axes=(1, 2))
 
-# Save CT as .im
-npi.ArrayToIm(pix.astype(np.float32), "ct.im")
+# Save attenuation map as .im
+npi.ArrayToIm(pix.astype(np.float32), "temp.im")
 
-# Reduce CT to 128x128x128 if necessary
+# Reduce attenuation map to 128x128x128 if necessary
 shape = pix.shape
 x_factor = shape[2] / 128
 y_factor = shape[1] / 128
 z_factor = shape[0] / 128
+new_bin_width = float(bin_width[0]) * x_factor
 
 if shape[0] != 128 or shape[1] != 128 or shape[2] != 128:
-    print("Downsampling CT")
-    if exists("ct_128.im"):
-        print("Removing previous ct_128.im")
-        cmd = "rm ct_128.im"
+    print("Downsampling attenuation map")
+    if exists("temp_128.im"):
+        print("Removing previous temp_128.im")
+        cmd = "rm temp_128.im"
         print("Running: " + cmd)
         runcmd(cmd,1)
 
-    cmd = f"collapse3d -a {x_factor} {y_factor} {z_factor} ct.im ct_128.im" # average the collapse, not sum, since it's not activity units
+    cmd = f"collapse3d -a {x_factor} {y_factor} {z_factor} temp.im temp_128.im" # average the collapse, not sum, since it's not activity units
     print("Running: " + cmd)
     runcmd(cmd,1)
+    waitall()
 
-    ct_name = "ct_128.im"
+    atn_name = "temp_128.im"
 else:
     print("No downsampling needed")
-    ct_name = "ct.im"
+    atn_name = "temp.im"
 
-# Create attenuation map from CT for each keV
-i = 0
-for keV in keVs:
-    i += 1
-    cmd = f"./hu2atn -e {keV} -s 0.48 {ct_name} atn.w{i}.im"
-    print("Running: " + cmd)
-    runcmd(cmd,1)
+# Convert attenuation map from linear attenuation/cm to attenuation per voxel
+pix = npi.ArrayFromIm(atn_name)
+pix = pix * new_bin_width / 10
+npi.ArrayToIm(pix.astype(np.float32), "atn.im")
 
-    # Update pixel spacing rows and columns in header
-    cmd = f"imsetinfo -i \"Pixel Spacing Rows\" \"4.8\" -i \"Pixel Spacing Cols\" \"4.8\" -i \"Slices Spacing\" \"-4.8\" -i \"Modality\" \"CT\" atn.w{i}.im"
-    print("Running: " + cmd)
-    runcmd(cmd,1)
+# Update pixel spacing rows and columns in header
+cmd = f"imsetinfo -i \"Pixel Spacing Rows\" \"{new_bin_width}\" -i \"Pixel Spacing Cols\" \"{new_bin_width}\" -i \"Slices Spacing\" \"-{new_bin_width}\" -i \"Modality\" \"CT\" atn.im"
+print("Running: " + cmd)
+runcmd(cmd,1)
 
 # Create symbolic links
 if exists("atn.w1i1.im"):
     cmd = "rm atn.w1i1.im"
     print("Running: " + cmd)
     runcmd(cmd,1)
-cmd = "ln -s atn.w1.im atn.w1i1.im"
+cmd = "ln -s atn.im atn.w1i1.im"
 print("Running: " + cmd)
 runcmd(cmd,1)
 
@@ -104,6 +97,10 @@ if exists("atn.w1i2.im"):
     cmd = "rm atn.w1i2.im"
     print("Running: " + cmd)
     runcmd(cmd,1)
-cmd = "ln -s atn.w1.im atn.w1i2.im"
+cmd = "ln -s atn.im atn.w1i2.im"
 print("Running: " + cmd)
+runcmd(cmd,1)
+
+# Remove temp .im images
+cmd = "rm temp*.im"
 runcmd(cmd,1)
